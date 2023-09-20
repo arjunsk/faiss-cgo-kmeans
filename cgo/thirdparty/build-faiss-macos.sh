4ff6892f820ceba63f9b1d60eff9c84dff4f4dea
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-09-19 22:06:47 -0700

    MacOS personal laptop build

--- cgo/thirdparty/build-faiss-macos.sh
+++ cgo/thirdparty/build-faiss-macos.sh
@@ -4,6 +4,7 @@ BRANCH="main"
 
 brew install libomp
 brew install cmake
+export CMAKE_PREFIX_PATH=/opt/homebrew/opt/openblas:/opt/homebrew/opt/libomp:/opt/homebrew
 git clone --recursive --branch $BRANCH https://github.com/facebookresearch/faiss.git libfaiss-src
 cd libfaiss-src
 cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DFAISS_ENABLE_C_API=ON -DBUILD_SHARED_LIBS=OFF -B build .
