8ed6f1898e93dc887283f3f1b2698bc00bb666ff
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-09-20 01:01:13 -0700

    rebuild

--- cgo/thirdparty/build-faiss-macos.sh
+++ cgo/thirdparty/build-faiss-macos.sh
@@ -7,8 +7,9 @@ brew install cmake
 export CMAKE_PREFIX_PATH=/opt/homebrew/opt/openblas:/opt/homebrew/opt/libomp:/opt/homebrew
 git clone --recursive --branch $BRANCH https://github.com/facebookresearch/faiss.git libfaiss-src
 cd libfaiss-src
-cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DFAISS_ENABLE_C_API=ON -DBUILD_SHARED_LIBS=OFF -B build .
-make -C build -j faiss
+git reset --hard  d87888b13e7eb339bb9c45825e9d20def6665171
+sudo cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DFAISS_ENABLE_C_API=ON -DBUILD_SHARED_LIBS=OFF -B build .
+sudo make -C build -j faiss
 sudo make -C build install
 
 arch=arm64
